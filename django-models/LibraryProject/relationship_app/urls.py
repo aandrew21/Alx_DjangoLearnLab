c329789bc099a061c5467c7d03f36c8ddcc5e7c7
Andrew Amponsah <amponsahandy1999@gmail.com>
2025-02-17 16:01:20 +0000

    updated

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,13 @@
 from .models import Library
 from django.urls import path
 from .views import list_books, LibraryDetailView
+from . import views
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
 ]
 
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,8 @@ from .models import Library
 from django.shortcuts import render
 from .models import Book, Library  # ✅ Make sure Library is imported here
 from django.views.generic.detail import DetailView  # ✅ Import DetailView
+from django.contrib.auth.decorators import user_passes_test
+from django.http import HttpResponseForbidden
 
 # Function-based view to list all books
 def list_books(request):
@@ -14,3 +16,27 @@ class LibraryDetailView(DetailView):
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
+# Function to check user role
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+# Admin view - accessible only by Admin users
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'admin_view.html')
+
+# Librarian view - accessible only by Librarian users
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'librarian_view.html')
+
+# Member view - accessible only by Member users
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'member_view.html')
