15b31b23d45d07557f96e240084ab43e232f89a5
Andrew Amponsah <amponsahandy1999@gmail.com>
2025-02-18 14:25:49 +0000

    updated

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -9,7 +9,7 @@ from django.http import HttpResponseForbidden
 from .models import UserProfile
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
-
+from django.contrib.auth.views import LoginView, LogoutView
 def list_books(request):
 	books = Book.objects.all()
 	return render(request, "relationship_app/list_books.html", {'books': books})
