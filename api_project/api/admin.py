f7879e2412386015e471bf274b7ef568c7a4127f
root <root@GH100LPC087.advans.ad>
2025-03-03 16:47:40 +0000

    updated

--- api_project/api/admin.py
+++ api_project/api/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import Book
+from rest_framework.authtoken.models import Token  # Import the Token model
 
+# Register the Book model
 admin.site.register(Book)
+
+# Register the Token model to manage tokens via the admin panel
+admin.site.register(Token)
+
 
 
 # Register your models here.
